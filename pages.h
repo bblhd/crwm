3e0630b298b674eab8443b3bbbad983fa527a904
Aydan <aydanwhitton@gmail.com>
2022-11-30 22:22:54 +1100

    fixed focusing issues for new windows

--- pages.h
+++ pages.h
@@ -14,7 +14,7 @@ struct Page {
 	} *columns;
 	struct Row {
 		uint16_t weight;
-		xcb_drawable_t window;
+		xcb_window_t window;
 		bool dontUnmanageYet;
 	} *rows;
 	uint16_t columnsLength, columnsMax;
@@ -31,22 +31,22 @@ struct ClientIndex {
 void setupPages();
 void cleanupPages();
 
-bool managed(xcb_drawable_t window, struct ClientIndex *index);
-void manage(xcb_drawable_t window, struct ClientIndex *index);
-void unmanage(xcb_drawable_t window);
+bool managed(xcb_window_t window, struct ClientIndex *index);
+void manage(xcb_window_t window, struct ClientIndex *index);
+void unmanage(xcb_window_t window);
 
-xcb_drawable_t lookUp(xcb_drawable_t window);
-xcb_drawable_t lookDown(xcb_drawable_t window);
-xcb_drawable_t lookRight(xcb_drawable_t window);
-xcb_drawable_t lookLeft(xcb_drawable_t window);
+xcb_window_t lookUp(xcb_window_t window);
+xcb_window_t lookDown(xcb_window_t window);
+xcb_window_t lookRight(xcb_window_t window);
+xcb_window_t lookLeft(xcb_window_t window);
 
-void moveUp(xcb_drawable_t window);
-void moveDown(xcb_drawable_t window);
-void moveLeft(xcb_drawable_t window);
-void moveRight(xcb_drawable_t window);
+void moveUp(xcb_window_t window);
+void moveDown(xcb_window_t window);
+void moveLeft(xcb_window_t window);
+void moveRight(xcb_window_t window);
 
-void changeWeights(xcb_drawable_t window, int16_t x, int16_t y);
-void sendPage(xcb_drawable_t window, uint16_t p);
+void changeWeights(xcb_window_t window, int16_t x, int16_t y);
+void sendPage(xcb_window_t window, uint16_t p);
 void switchPage(uint16_t p);
 
 #endif
